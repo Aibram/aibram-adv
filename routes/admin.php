f1a13ea671d7d028ab1fc1d43f977a52c77d7687
Mostafa <mos.tafa93@yahoo.com>
2021-05-14 14:36:59 +0200

    Check if working in cpanel

--- routes/admin.php
+++ routes/admin.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::name('admin.')->group(function(){
     Route::middleware('guest:admin')->group(function(){
-        //login route
         Route::get('/login',[LoginController::class,'showLoginForm'])->name('login');
         Route::post('/login',[LoginController::class,'login'])->name('loginPost');
     });
