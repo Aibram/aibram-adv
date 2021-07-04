ad827efc36ba0e7dec9285e473940b67cd0c1d5f
Mostafa <mos.tafa93@yahoo.com>
2021-07-04 02:26:24 +0200

    Update Comment Notification

--- app/Notifications/CommentAddFrom.php
+++ app/Notifications/CommentAddFrom.php
@@ -2,9 +2,11 @@
 
 namespace App\Notifications;
 
+use Illuminate\Database\Eloquent\Model;
+
 class CommentAddFrom extends BaseNotification
 {
-    public function __construct(array $data)
+    public function __construct(Model $data)
     {
         $this->data = $data;
     }
